e59abe6c189b6bc960ec318cdda6e8760f94399f
Joseph Outten <jho5ze@virginia.edu>
2021-03-09 17:20:10 -0500

    First run with Stellar Graph. In network example from the docs

---
+++ BGCs/scripts/pgfams_and_pfams_from_tsvs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# $1=first_pass_split_0_train_with_pfams.tsv
+# $2=first_pass_split_0_test_with_pfams.tsv
+
+rg_str="PGF|PF"
+
+for gid in $(rg $rg_str $1 | cut -f 1 | sort -u | cut -f 2 -d '"'); do echo $(rg $gid $1 | rg $rg_str | cut -f 18 | tr -d '"' | tr "\n" " ") > ${gid}_pgfams_and_pfams.txt; done
+
+for gid in $(rg $rg_str $2 | cut -f 1 | sort -u | cut -f 2 -d '"'); do echo $(rg $gid $2 | rg $rg_str | cut -f 18 | tr -d '"' | tr "\n" " ") > ${gid}_pgfams_and_pfams.txt; done
