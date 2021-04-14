1c6481606b9d06ae248c983972a5ef9f2d322b3b
Joseph Outten <jho5ze@virginia.edu>
2021-04-14 16:17:15 -0400

    Finished first pass on Houston with some missing dates. Still need to optimize the entopy calculation, exp. get_complement_nodes()

--- covid/scripts/msa_to_graph.py
+++ covid/scripts/msa_to_graph.py
@@ -8,7 +8,7 @@ from copy import deepcopy
 import sys
 import os
 
-def add_root(ggraph, num_genomes):
+def add_root(ggraph, num_genomes, genomes):
     no_in_nodes = []
     for node in ggraph.nodes():
         if ggraph.in_degree(node) == 0:
@@ -16,77 +16,135 @@ def add_root(ggraph, num_genomes):
     if len(no_in_nodes) == 1:
         return no_in_nodes[0]
     else:
-        ggraph.add_node("root", sequence="N", num_genomes=num_genomes)
+        ggraph.add_node("root", sequence="N", num_genomes=num_genomes, genomes=genomes)
         for node in no_in_nodes:
             ggraph.add_edge("root", node)
         return "root", no_in_nodes
 
-def get_complement_nodes(ggraph, x_node): #, num_genomes):
-    #Could benefit from some indexing
-    #Could optimize by finding the consensus nodes and stopping 
-    #     early there since all above or below are "implied"
-    #     but would need to potentially iterate over the nodes 
-    #     again since switching from negation to inclusion
-    node_states = {node:True for node in ggraph.nodes}
-        
-    def follow_ancestors(node):
-        if node_states[node] or node == x_node: #stop if node already visited
-            node_states[node] = False
-            for anc_node in ggraph.predecessors(node):
-                follow_ancestors(anc_node)
+def get_complement_nodes(ggraph, x_node, induced_nodes_dict, num_genomes):
+    x_genomes = ggraph.nodes[x_node]["genomes"]
+    
+    if len(x_genomes) == num_genomes: #All genomes included so all nodes induced
+        return []
+    
+    induced_nodes = set().union(*[induced_nodes_dict[genome] for genome in x_genomes])
+    all_nodes = set(ggraph.nodes())
+    
+    complement_nodes = all_nodes - induced_nodes
+    return complement_nodes
+    
+# def get_complement_nodes(ggraph, x_node):
+#     y_nodes = []
+    
+#     x_genomes = set(ggraph.nodes[x_node]["genomes"])
+    
+#     for node in ggraph.nodes():
+#         node_genomes = set(ggraph.nodes[node]["genomes"])
+#         if len(node_genomes & x_genomes) == 0:
+#             y_nodes.append(node)
             
-    def follow_descendants(node):
-        if node_states[node] or node == x_node: #stop if node already visited
-            node_states[node] = False
-            for dec_node in ggraph.successors(node):
-                follow_descendants(dec_node)
+#     return y_nodes
     
-    follow_ancestors(x_node)
-    follow_descendants(x_node)
+# def get_complement_nodes(ggraph, x_node): #, num_genomes):
+#     #Could benefit from some indexing
+#     #Could optimize by finding the consensus nodes and stopping 
+#     #     early there since all above or below are "implied"
+#     #     but would need to potentially iterate over the nodes 
+#     #     again since switching from negation to inclusion
+#     node_in_y_states = {node:True for node in ggraph.nodes}
+#     node_visited_states = {node:False for node in ggraph.nodes}
+#     genomes = set(ggraph.nodes[x_node]["genomes"])
+        
+#     def follow_ancestors(node):
+#         if not node_visited_states[node] or node == x_node: #stop if node already visited
+#             node_visited_states[node] = True
+#             if len(set(ggraph.nodes[node]["genomes"]) & genomes) > 0:
+#                 node_in_y_states[node] = False
+#                 for anc_node in ggraph.predecessors(node):
+#                     follow_ancestors(anc_node)
+            
+#     def follow_descendants(node):
+#         if not node_visited_states[node] or node == x_node: #stop if node already visited
+#             node_visited_states[node] = True
+#             if len(set(ggraph.nodes[node]["genomes"]) & genomes) > 0:
+#                 node_in_y_states[node] = False
+#                 for dec_node in ggraph.successors(node):
+#                     follow_descendants(dec_node)
     
-    return [node for node in node_states if node_states[node]]
+#     follow_ancestors(x_node)
+#     follow_descendants(x_node)
+    
+#     return [node for node in node_in_y_states if node_in_y_states[node]]
 
-def calc_node_chars(ggraph, node):
+def get_sequence_genome_weight(ggraph, node):
     return len(ggraph.nodes[node]["sequence"]) * ggraph.nodes[node]["num_genomes"]
 
-def calc_uncertainty(ggraph, num_chars=None): #, num_genomes):
+def get_genome_weight(ggraph, node):
+    return ggraph.nodes[node]["num_genomes"]
+
+def get_sequence_weight(ggraph, node):
+    return len(ggraph.nodes[node]["sequence"])
+
+def get_weight(ggraph, node):
+    return 1
+
+def calc_uncertainty(ggraph, induced_nodes_dict, weigh_on_sequence=True, weigh_on_genomes=True): #, num_genomes):
     
-    char_per_node = False
+    """
+    Calculates the uncertainty/entropy of the genome graph using configurable weighing schemes
     
-    if num_chars is None: #Use each node as a character
-        num_chars = len(graph.nodes())
-        char_per_node = True
+    ToDo:
+        Determine if the genome weighing by itself should be, e.g. for the graph, the sum of the number of genomes in each node
+    """
     
+    num_genomes = len(induced_nodes_dict.keys())
+    
+    if weigh_on_sequence and num_chars is None:
+        raise ValueError("If using sequence weighing, must provide the total number of characters in the graph")
+    
+    if weigh_on_genomes and weigh_on_sequence:
+        func = get_sequence_genome_weight
+    elif weigh_on_genomes:
+        func = get_genome_weight
+    elif weigh_on_sequence:
+        func = get_sequence_weight
+    else:
+        func = get_weight
+        
+    graph_weight = sum([func(ggraph, node) for node in ggraph.nodes])
     uncert = 0
     
+    terms = dict()
+    
     for x_node in ggraph.nodes():
-        x_node_info = ggraph.nodes[x_node]
-        x_node_characters = 1 if char_per_node else calc_node_chars(ggraph, x_node)
         
-        x_uncert = x_node_characters / num_chars
+        x_weight = func(ggraph, x_node)
+        
+        x_uncert = x_weight / graph_weight
         
         #For the first term
         uncert -= x_uncert * log( x_uncert , 2)
         
         #For the second term
-        y_nodes = get_complement_nodes(ggraph, x_node)
-        y_set_chars = len(y_nodes) if char_per_node else sum([calc_node_chars(ggraph, y_node) for y_node in y_nodes])
+#         y_nodes = get_complement_nodes(ggraph, x_node)
+        y_nodes = get_complement_nodes(ggraph, x_node, induced_nodes_dict, num_genomes)
+        y_set_weight = sum([func(ggraph, y_node) for y_node in y_nodes])
         for y_node in y_nodes:
             
-            y_node_characters = 1 if char_per_node else calc_node_chars(ggraph, y_node)
-            y_uncert = ( y_node_characters / y_set_chars )
+            y_node_weight = func(ggraph, y_node)
+            y_uncert = ( y_node_weight / y_set_weight )
             
             uncert -= x_uncert * y_uncert * log( y_uncert , 2)
-            
     return uncert
 
 def generate_nx_from_msa(seq_paths):
     num_chars = 0
     genome_len = 34742
-
+    genomes =[]
+    
     graph = nx.DiGraph()
     for ix in range(genome_len):
-        for letter in ["A","T","C","G","N"]:
+        for letter in ["A","T","C","G"]: #,"N"]:
             graph.add_node(f"{ix}_{letter}", genomes=[], sequence=letter, num_genomes=0)
 
     for seq in seq_paths:
@@ -94,16 +152,17 @@ def generate_nx_from_msa(seq_paths):
             for line in src.readlines():
                 line = line.strip()
                 if line[0] == ">":
-                    genome = line[1:] #.split("/")[2]
+                    genome = line[1:].split("/")[2]
+                    genomes.append(genome)
                 else:
 
                     prev_node = None
                     num_chars += len(line)
-                    for ix, char in enumerate(line): #[34000:35000]):
-                        if char == "-":
-                            char = "N" #Preserves overall length of genome graph
-                        if char not in ["A","T","C","G","N"]:
-                            char = "N"
+                    for ix, char in enumerate(line): 
+#                         if char == "-":
+#                             char = "N" #Preserves overall length of genome graph
+                        if char not in ["A","T","C","G"]: #,"N"]:
+#                             char = "N"
                             continue
                         node = f"{ix}_{char}"
                         graph.nodes[node]["genomes"].append(genome)
@@ -118,7 +177,7 @@ def generate_nx_from_msa(seq_paths):
             
     graph.remove_nodes_from(empty_nodes)
     
-    return graph, num_chars
+    return graph, num_chars, genomes
 
 def collapse_graph(graph):
     new_graph = deepcopy(graph)
@@ -157,19 +216,42 @@ uncert_file = sys.argv[2]
 seq_paths = sys.argv[3:]
 
 num_genomes = len(seq_paths)
-graph, num_chars = generate_nx_from_msa(seq_paths)
-add_root(graph, num_genomes)
+graph, num_chars, genomes = generate_nx_from_msa(seq_paths)
+add_root(graph, num_genomes, genomes)
 collapsed_graph = collapse_graph(graph)
 
-full_uncert = calc_uncertainty(collapsed_graph, num_chars=num_chars)
-collapsed_uncert = calc_uncertainty(collapsed_graph)
+columns = []
+values = []
+
+graph_genome_induced_nodes = {genome:set() for genome in genomes}
+collapsed_graph_genome_induced_nodes = {genome:set() for genome in genomes}
+for node in graph.nodes():
+    node_genomes = graph.nodes[node]["genomes"]
+    for genome in genomes:
+        if genome in node_genomes:
+            graph_genome_induced_nodes[genome].add(node)
+            
+for node in collapsed_graph.nodes():
+    node_genomes = collapsed_graph.nodes[node]["genomes"]
+    for genome in genomes:
+        if genome in node_genomes:
+            collapsed_graph_genome_induced_nodes[genome].add(node) 
+
+# for G, graph_name in zip([graph, collapsed_graph], ["full", "collapsed"]):
+for G, graph_name, induced_nodes in zip([collapsed_graph], ["collapsed"], [collapsed_graph_genome_induced_nodes]):
+    for seqs in range(2):
+        seq_name = f"_sequences_{seqs}"
+        for genomes in range(2):
+            genome_name = f"_genomes_{genomes}"
+            columns.append(graph_name + seq_name + genome_name)
+            values.append(str(calc_uncertainty(G, induced_nodes, weigh_on_sequence=seqs, weigh_on_genomes=genomes)))
 
 graph_name = str(graph_out_path).split("/")[-1]
 graph_path = graph_out_path / (graph_name + ".gpkl")
 nx.write_gpickle(collapsed_graph, graph_path)
 if not os.path.exists(uncert_file):
     with open(uncert_file, "w") as dest:
-        dest.write("graph_name,full_uncertainty,collapsed_uncertainty\n")
+        dest.write(f"graph_name,{','.join(columns)}\n")
         
 with open(uncert_file, "a") as dest:
-    dest.write(f"{graph_name},{full_uncert},{collapsed_uncert}\n")
+    dest.write(f"{graph_name},{','.join(values)}\n")
